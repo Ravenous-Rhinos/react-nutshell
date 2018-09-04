9adf7a59b49a5f3c7fe16020b45ec95ee0ef3ed5
ThraxHamilton <mmcclenton16@gmail.com>
2018-09-04 13:59:12 -0500

    tasks adding

---
+++ src/components/task/TaskForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react"
+
+export default class TaskForm extends Component {
+    // Set initial state
+    state = {
+        task: "",
+        details: "",
+        due: "",
+    }
+
+    // Update state whenever an input field is edited
+    handleFieldChange = evt => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    /*
+        Local method for validation, creating animal object, and
+        invoking the function reference passed from parent component
+     */
+    constructNewTask = evt => {
+        evt.preventDefault()
+        if (this.state.task === "") {
+            window.alert("Please select a caretaker")
+        } else {
+            const newTask = {
+                name: this.state.task,
+            }
+
+            // Create the employee and redirect user to employee list
+            this.props.addTask(newTask, 'tasks').then(() => this.props.history.push("/tasks"))
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <form className="TaskForm">
+                    <div className="form-group">
+                        <label htmlFor="newTask">New Task</label>
+                        <input type="text" required="true"
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="task"
+                            placeholder="I Need To..." />
+                    </div>
+
+                    <button type="submit" onClick={this.constructNewTask} className="btn btn-primary">Submit</button>
+                </form>
+            </React.Fragment>
+        )
+    }
+}
---
+++ src/components/task/TaskList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+
+class TaskList extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="taskButton">
+                    <button type="button"
+                        className="btn btn-success"
+                        onClick={() => {
+                            this.props.history.push("/tasks/new")
+                        }
+                        }>
+                       Add Task
+                </button>
+                </div>
+                <section className="tasks">
+                    {
+                        this.props.tasks.map(task =>
+                            <div key={task.id} className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    {task.name}
+                                {task.phone}
+                                <Link className="nav-link" to={`/tasks/${task.id}`}>Details</Link>
+                                    <a href="#"
+                                        onClick={() => this.props.deleteTask(task.id)}
+                                        className="card-link">Delete</a>
+                                </h5>
+                            </div>
+                            </div>
+                        )
+                    }
+                </section>
+                </React.Fragment>
+                )
+            }
+        }
+        
+export default TaskList
